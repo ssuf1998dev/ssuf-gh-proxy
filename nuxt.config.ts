8e27c9d58b5b512adaddc3ce2f653c40182c2a9e
ssuf1998dev <ssuf1998dev@126.com>
2024-07-07 10:28:32 +0800

    feat(server): basic auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     "unplugin-icons/nuxt",
     "@nuxtjs/i18n",
     "@vueuse/nuxt",
+    "@kgierke/nuxt-basic-auth",
   ],
   css: [
     "normalize.css",
@@ -20,5 +21,14 @@ export default defineNuxtConfig({
   },
   i18n: {
     vueI18n: "locales/index.ts",
+  },
+  basicAuth: {
+    enabled: process.env.SGP_DISABLE_AUTH !== "true",
+    users: [
+      {
+        username: process.env.SGP_USERNAME || "admin",
+        password: process.env.SGP_PASSWORD || "admin",
+      },
+    ],
   },
 });
